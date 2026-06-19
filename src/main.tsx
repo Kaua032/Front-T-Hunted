2eb7626e670a85d237115095f9db16f8fb5e112a
kaua <kauaxavier032@gmail.com>
2026-06-18 21:28:19 -0300

    feat: update dependencies and implement global styles with styled-components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React, { useEffect } from 'react'
+import ReactDOM from "react-dom/client";
+import { GlobalStyled } from "./GlobalStyled.jsx";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <></>,
+  },
+  {
+    path: "/home",
+    element: <></>,
+  },
+  {
+    path: "/teachers",
+    element: <></>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GlobalStyled />
+  </React.StrictMode>
+);
