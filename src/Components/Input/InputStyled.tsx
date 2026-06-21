d553bb62f5fae42d921df9e4fd0149581e537901
kaua <kauaxavier032@gmail.com>
2026-06-21 20:22:49 -0300

    feat: styling input and registration components; adjusting sizes and responsiveness.

--- src/Components/Input/InputStyled.tsx
+++ src/Components/Input/InputStyled.tsx
@@ -1,17 +1,21 @@
 import styled from "styled-components";
 
 export const InputStyled = styled.input`
-    width: 300px;
-    height: 30px;
+  width: 300px;
+  height: 30px;
 
-    border: #D9D9D9 1px solid;
-    border-radius: 5px;
-    padding-left: 10px;
+  border: #d9d9d9 1px solid;
+  border-radius: 5px;
+  padding-left: 10px;
 
-    &::placeholder{
-        font-family: Roboto;
-        color: #D9D9D9;
-        font-size: 12px;
-        font-weight: 300;
-    }
+  &::placeholder {
+    font-family: Roboto;
+    color: #d9d9d9;
+    font-size: 12px;
+    font-weight: 300;
+  }
+
+  @media (max-width: 450px) {
+    width: 95%;
+  }
 `;
